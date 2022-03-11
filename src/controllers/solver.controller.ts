12721e35cf84417ae8a939f8f4a73fd186256104
orla11 <orlandif.mail@gmail.com>
2022-03-11 23:27:11 +0100

    rimosso commento

--- src/controllers/solver.controller.ts
+++ src/controllers/solver.controller.ts
@@ -1,9 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import SolverService from "../services/solver.service";
 
-/* TODO: move logic into services in order to separate
-business logic from the controllers */
-
 const solveBoard = async (req: Request, res: Response): Promise<void> => {
 	SolverService.solveBoard([[]]);
 	//res.status(200).json({ board });
