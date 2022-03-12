673d47088d360d155cd5d4ac74e74fbb33c41406
CristianoCiuti <cristiano.ciuti@gmail.com>
2022-03-12 15:29:28 +0100

    +Angular Material, + Grid component

---
+++ app/src/app/grid/grid.component.ts
@@ -0,0 +1,75 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface Size{
+  height: number,
+  width: number
+}
+
+@Component({
+  selector: 'sudoku-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.scss']
+})
+export class GridComponent implements OnInit {
+
+  @Input('size') size: number = 9;
+  @Input('section-size') sectionSize: string = '3x3';
+
+  private _size: Size = {
+    height: 9,
+    width: 9
+  };
+  private _sectionSize: Size = {
+    height: 3,
+    width: 3
+  } 
+
+  constructor() { 
+    let inputSize = { height: this.size, width: this.size };
+    let inputSectionSize = this.convertSectionSize(this.sectionSize);
+
+    this._size = this.validateInput(inputSize) ? inputSize : this._size;
+    this._sectionSize = this.validateInput(inputSectionSize, this._size) ? inputSectionSize : this._sectionSize;
+  }
+
+  ngOnInit(): void {
+  }
+  
+  private validateInput(value: Size, container?: Size) : boolean {
+    let result = false;
+    // Se passo un container verifico che sia contenuto nel container
+    if (container) {
+      result = container.height % value.height  === 0 
+      && 
+      container.width % value.width === 0;
+    }
+    // se non passo il container allora é la validazione del container stesso
+    // che deve essere un quadrato di dimensioni 6,9,16
+    else{
+      result = value.height === value.width 
+      && 
+      [6,9,16].includes(value.height);
+    }
+
+    if (!result)
+      console.error(`Input validation error: size=${JSON.stringify(value)}, container=${JSON.stringify(container)}`, value, container);
+    
+    return result;
+  }
+
+  private convertSectionSize(value: string) : Size{
+    if (new RegExp('^\s*[0-9]{1,2}\s*x\s*[0-9]{1,2}\s*$').test(value)){
+      let split = value
+        .replace(new RegExp('\s'), '')
+        .split(new RegExp('[x,X]'));
+      
+      return {
+        width: Number(split[0]),
+        height: Number(split[1])
+      }
+    }
+
+    return this._sectionSize;
+  }
+
+}
