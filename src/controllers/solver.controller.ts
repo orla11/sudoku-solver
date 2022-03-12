da47108d5fdf380fbf0299ff0728dfa1a3e6030b
orla11 <orlandif.mail@gmail.com>
2022-03-12 01:08:49 +0100

    solver controller development

--- src/controllers/solver.controller.ts
+++ src/controllers/solver.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import { IBoard } from "../interfaces/IBoard";
 import SolverService from "../services/solver.service";
 
-const solveBoard = async (req: Request, res: Response): Promise<void> => {
-	SolverService.solveBoard([[]]);
-	//res.status(200).json({ board });
+const solveBoard = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> => {
+	let request: IBoard = req.body as IBoard;
+	let solution = await SolverService.solveBoard(request.board);
+	res.status(200).json({ board: solution });
 };
 
 export { solveBoard };
