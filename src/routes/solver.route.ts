7cebdbf5d947e6a5e2635443f88cfc7e969f2acf
orla11 <orlandif.mail@gmail.com>
2022-03-12 01:09:15 +0100

    removed comment

--- src/routes/solver.route.ts
+++ src/routes/solver.route.ts
@@ -4,6 +4,5 @@ import { solveBoard } from "../controllers/solver.controller";
 const router: Router = Router();
 
 router.route("/").post(solveBoard);
-//router.route("/:boardId").get(getHint);
 
 export default router;
